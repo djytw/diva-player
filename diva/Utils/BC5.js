3e1abae52c30cb2f2b95f6820249973ad68d432d
djytw <djytw@live.com>
2020-01-26 02:13:09 +0100

    add support for dc5 texture that w!=h

--- diva/Utils/BC5.js
+++ diva/Utils/BC5.js
@@ -17,14 +17,14 @@ class BC5{
 
         return (~~(~~(pos / 16) / (size / 4)) * 4 + (~~((pos % 16) / 4))) * size + ((~~(pos / 16) % (size / 4)) * 4 + (pos % 16) % 4);
     }
-    static decode(data, size){
+    static decode(data, width, height){
         if (!data instanceof Uint8Array){
             return ;
         }
-        var ret = new Uint8Array(size*size*3);
+        var ret = new Uint8Array(width*height*3);
         var i, j;
         var r = new Array(8);
-        for (i = 0; i < size*size ; i+=16){
+        for (i = 0; i < width*height ; i+=16){
             r[0] = data[i];
             r[1] = data[i+1];
             if (r[0] > r[1]){
@@ -46,13 +46,13 @@ class BC5{
             }
             var td = (data[i+2] << 0) + (data[i+3] << 8) + (data[i+4] << 16);
             for (j = 0; j < 8; j ++){
-                ret[this.tile2linear(i+j,size)*3] = r[td & 7];
+                ret[this.tile2linear(i+j,width)*3] = r[td & 7];
                 //if(i+j==0)debugger;
                 td>>=3;
             }
             td = (data[i+5] << 0) + (data[i+6] << 8) + (data[i+7] << 16);
             for (j = 8; j < 16; j ++){
-                ret[this.tile2linear(i+j,size)*3] = r[td & 7];
+                ret[this.tile2linear(i+j,width)*3] = r[td & 7];
                 td>>=3;
             }
             r[0] = data[i+8];
@@ -76,26 +76,32 @@ class BC5{
             }
             td = (data[i+10] << 0) + (data[i+11] << 8) + (data[i+12] << 16);
             for (j = 0; j < 8; j ++){
-                ret[this.tile2linear(i+j,size)*3+1] = r[td & 7];
-                ret[this.tile2linear(i+j,size)*3+2] = 255;
+                ret[this.tile2linear(i+j,width)*3+1] = r[td & 7];
+                ret[this.tile2linear(i+j,width)*3+2] = 255;
                 td>>=3;
             }
             td = (data[i+13] << 0) + (data[i+14] << 8) + (data[i+15] << 16);
             for (j = 8; j < 16; j ++){
-                ret[this.tile2linear(i+j,size)*3+1] = r[td & 7];
-                ret[this.tile2linear(i+j,size)*3+2] = 255;
+                ret[this.tile2linear(i+j,width)*3+1] = r[td & 7];
+                ret[this.tile2linear(i+j,width)*3+2] = 255;
                 td>>=3;
             }
         }
         
-        //var newfile = new PNG({width:size,height:size});
-        //for(i=0;i<size*size;i++){
+        //var newfile = new PNG({width:width,height:height});
+        //for(i=0;i<width*height;i++){
         //    newfile.data[i*4] = ret[i*3];
         //    newfile.data[i*4+1] = ret[i*3+1];
         //    newfile.data[i*4+2] = ret[i*3+2];
         //    newfile.data[i*4+3] =0xff;
         //}
         //var buffer = PNG.sync.write(newfile, { colorType: 6 });
+        //var a = document.createElement('a');
+        //var file = new Blob([ buffer ], { type: 'image/png' });
+        //
+        //a.href = URL.createObjectURL(file);
+        //a.download = 'bla';
+        //a.click();
         return ret;
     }
 }
